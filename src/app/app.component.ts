a990c779df238d477dc179637cd3b6e241996d73
jdickinson <jdickinson@statebridgecompany.com>
2017-02-28 15:49:03 -0700

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component ,  OnInit} from '@angular/core';
+
+//import * as fin from 'finesse';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(){
+    this.uri = "https://uccx-001-app-prod.statebridgecompany.com:8445";
+  }
+  uri: string;
   title = 'app works!';
+  connect(){
+
+
+
+     finesse.ClientServices.init({host: this.uri, restHost: this.uri, id: "jdickinson", password: "4"});
+  }
+  ngOnInit(){
+
+    this.title = 'test';
+    // fi.gadget.Config = {host: this.uri, restHost: this.uri, id: "jdickinson", password: "isFriday!4"}
+    // fi.clientservices.ClientServices.init(fi.gadget.Config);
+  }
 }
+
+
---
+++ src/finesse.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by jdickinson on 2/28/2017.
+ */
+declare var finesse: any;
+/*
+ @foxjazz so what you need to do is import it in your application somewhere like
+ import 'somelib'like your main.ts
+ then in the file you want to use it
+ declare let somelib:any;
+ then you can use it in that file without issues
+ you will need to do declare let somelib:any; in every file you need to use it
+
+ */
